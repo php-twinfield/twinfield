81c73bbef7ef13706af7e7f7a4ec4b4d8d7c4cdd
Leon Rowland <leon@rowland.nl>
2013-04-11 14:00:10 +0200

    MUCH Cleaner now

--- src/Pronamic/Twinfield/Customer/Mapper/CustomerMapper.php
+++ src/Pronamic/Twinfield/Customer/Mapper/CustomerMapper.php
@@ -10,62 +10,95 @@ class CustomerMapper {
 	public static function map(Response $response) {
 		$responseDOM = $response->getResponseDocument();
 		
+		$customerTags = array(
+			'code' => 'setID',
+			'uid' => 'setUID',
+			'name' => 'setName',
+			'inuse' => 'setInUse',
+			'behaviour' => 'setBehaviour',
+			'touched' => 'setTouched',
+			'beginperiod' => 'setBeginPeriod',
+			'endperiod' => 'setEndPeriod',
+			'endyear' => 'setEndYear',
+			'website' => 'setWebsite',
+			'cocnumber' => 'setCocNumber',
+			'vatnumber' => 'setVatNumber',
+			'editdimensionname' => 'setEditDimensionName'
+		);
+		
 		// Generate new customer object
 		$customer = new Customer();
-
-		$customer
-				->setID($responseDOM->getElementsByTagName('code')->item(0)->textContent)
-				->setUID($responseDOM->getElementsByTagName('uid')->item(0)->textContent)
-				->setName($responseDOM->getElementsByTagName('name')->item(0)->textContent)
-				->setInUse($responseDOM->getElementsByTagName('inuse')->item(0)->textContent)
-				->setBehaviour($responseDOM->getElementsByTagName('behaviour')->item(0)->textContent)
-				->setTouched($responseDOM->getElementsByTagName('touched')->item(0)->textContent)
-				->setBeginPeriod($responseDOM->getElementsByTagName('beginperiod')->item(0)->textContent)
-				->setBeginYear($responseDOM->getElementsByTagName('beginyear')->item(0)->textContent)
-				->setEndPeriod($responseDOM->getElementsByTagName('endperiod')->item(0)->textContent)
-				->setEndYear($responseDOM->getElementsByTagName('endyear')->item(0)->textContent)
-				->setWebsite($responseDOM->getElementsByTagName('website')->item(0)->textContent)
-				->setCocNumber($responseDOM->getElementsByTagName('cocnumber')->item(0)->textContent)
-				->setVatNumber($responseDOM->getElementsByTagName('vatnumber')->item(0)->textContent)
-				->setEditDimensionName($responseDOM->getElementsByTagName('editdimensionname')->item(0)->textContent);
-
+		
+		// Loop through all the tags
+		foreach($customerTags as $tag => $method) {
+			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
+			
+			if(isset($_tag) && isset($_tag->textContent))
+				$customer->$method($_tag->textContent);
+		} 
+		
+		$financialTags = array(
+			'duedays' => 'setDueDays',
+			'payavailable' => 'setPayAvailable',
+			'paycode' => 'setPayCode',
+			'ebilling' => 'setEBilling',
+			'ebillmail' => 'setEBillMail'
+		);
+		
 		// Financial elements
 		$financialElement = $responseDOM->getElementsByTagName('financials')->item(0);
-
-		$customer
-				->setDueDays($financialElement->getElementsByTagName('duedays')->item(0)->textContent)
-				->setPayAvailable($financialElement->getElementsByTagName('payavailable')->item(0)->textContent)
-				->setPayCode($financialElement->getElementsByTagName('paycode')->item(0)->textContent)
-				->setEBilling($financialElement->getElementsByTagName('ebilling')->item(0)->textContent)
-				->setEBillMail($financialElement->getElementsByTagName('ebillmail')->item(0)->textContent);
-
+		
+		foreach($financialTags as $tag => $method) {
+			$_tag = $financialElement->getElementsByTagName($tag)->item(0);
+			
+			if(isset($_tag) && isset($_tag->textContent))
+				$customer->$method($_tag->textContent);
+		}
+		
+		// Element tags and their methods for address
+		$address_tags = array(
+			'name'		 => 'setName',
+			'country'	 => 'setCountry',
+			'city'		 => 'setCity',
+			'postcode'	 => 'setPostcode',
+			'telephone'	 => 'setTelephone',
+			'telefax'	 => 'setFax',
+			'email'		 => 'setEmail',
+			'field1'	 => 'setField1',
+			'field2'	 => 'setField2',
+			'field3'	 => 'setField3',
+			'field4'	 => 'setField4',
+			'field5'	 => 'setField5',
+			'field6'	 => 'setField6',
+		);
+		
 		// Build address entries
 		foreach($responseDOM->getElementsByTagName('address') as $addressDOM) {
+			// Make a new temp customeraddress class
 			$temp_address = new CustomerAddress();
+			
+			// Set the default properties ( id, type, default )
 			$temp_address
 					->setID($addressDOM->getAttribute('id'))
 					->setType($addressDOM->getAttribute('type'))
-					->setDefault($addressDOM->getAttribute('default'))
-					->setName($addressDOM->getElementsByTagName('name')->item(0)->textContent)
-					->setCountry($addressDOM->getElementsByTagName('country')->item(0)->textContent)
-					->setCity($addressDOM->getElementsByTagName('city')->item(0)->textContent)
-					->setPostcode($addressDOM->getElementsByTagName('postcode')->item(0)->textContent)
-					->setTelephone($addressDOM->getElementsByTagName('telephone')->item(0)->textContent)
-					->setFax($addressDOM->getElementsByTagName('telefax')->item(0)->textContent)
-					->setEmail($addressDOM->getElementsByTagName('email')->item(0)->textContent)
-					->setField1($addressDOM->getElementsByTagName('field1')->item(0)->textContent)
-					->setField2($addressDOM->getElementsByTagName('field2')->item(0)->textContent)
-					->setField3($addressDOM->getElementsByTagName('field3')->item(0)->textContent)
-					->setField4($addressDOM->getElementsByTagName('field4')->item(0)->textContent)
-					->setField5($addressDOM->getElementsByTagName('field5')->item(0)->textContent)
-					->setField6($addressDOM->getElementsByTagName('field6')->item(0)->textContent);
-
+					->setDefault($addressDOM->getAttribute('default'));
+			
+			// Loop through the element tags. Determine if it exists and set it if it does
+			foreach($address_tags as $tag => $method) {
+				$_tag = $addressDOM->getElementsByTagName($tag)->item(0);
+				
+				// Check if the tag is set, and its content is set, to prevent DOMNode errors
+				if(isset($_tag) && isset($_tag->textContent))
+					$temp_address->$method($_tag->textContent);
+			}
+			
+			// Add the address to the customer
 			$customer->addAddress($temp_address);
+			
+			// Clean that memory!
 			unset($temp_address);
-
 		}
 
 		return $customer;
-
 	}
 }
