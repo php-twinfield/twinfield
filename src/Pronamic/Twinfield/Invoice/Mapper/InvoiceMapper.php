ff68d1ac107fa74dbe15dc7347ee3ed712a1c353
Leon Rowland <leon@rowland.nl>
2013-04-15 08:51:00 +0200

    InvoiceMapper updated to new method

--- src/Pronamic/Twinfield/Invoice/Mapper/InvoiceMapper.php
+++ src/Pronamic/Twinfield/Invoice/Mapper/InvoiceMapper.php
@@ -10,56 +10,78 @@ use \Pronamic\Twinfield\Customer\Customer;
 
 class InvoiceMapper {
 	public static function map( Response $response ) {
-
 		$responseDOM = $response->getResponseDocument();
-
+		
+		$invoiceTags = array(
+			'office' => 'setOffice',
+			'invoicetype' => 'setInvoiceType',
+			'invoicenumber' => 'setInvoiceNumber',
+			'invoicedate' => 'setInvoiceDate',
+			'duedate' => 'setDueDate',
+			'bank' => 'setBank',
+			'invoiceaddressnumber' => 'setInvoiceAddressNumber',
+			'deliveraddressnumber' => 'setDeliverAddressNumber',
+			'period' => 'setPeriod',
+			'currency' => 'setCurrency',
+			'status' => 'setStatus',
+			'paymentmethod' => 'setPaymentMethod'
+		);
+		
+		$customerTags = array(
+			'customer' => 'setID'
+		);
+		
 		// Generate new Invoice
 		$invoice = new Invoice();
+		
+		foreach($invoiceTags as $tag => $method) {
+			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
+			
+			if(isset($_tag) && isset($_tag->textContent))
+				$invoice->$method($_tag->textContent);
+		}
+		
 		$customer = new Customer();
-		$customer->setID( $responseDOM->getElementsByTagName( 'customer' )->item(0)->textContent );
+		
+		foreach($customerTags as $tag => $method) {
+			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
+			
+			if(isset($_tag) && isset($_tag->textContent))
+				$customer->$method($_tag->textContent);
+		}
+		
+		$invoice->setCustomer( $customer );
 
-		// Set the invoice properties
-		$invoice->setOffice( $responseDOM->getElementsByTagName( 'office' )->item(0)->textContent )
-				->setInvoiceType( $responseDOM->getElementsByTagName( 'invoicetype' )->item(0)->textContent )
-				->setInvoiceNumber( $responseDOM->getElementsByTagName( 'invoicenumber' )->item(0)->textContent )
-				->setInvoiceDate( $responseDOM->getElementsByTagName( 'invoicedate' )->item(0)->textContent )
-				->setDueDate( $responseDOM->getElementsByTagName( 'duedate' )->item(0)->textContent )
-				->setBank( $responseDOM->getElementsByTagName( 'bank' )->item(0)->textContent )
-				->setInvoiceAddressNumber( $responseDOM->getElementsByTagName( 'invoiceaddressnumber' )->item(0)->textContent )
-				->setDeliverAddressNumber( $responseDOM->getElementsByTagName( 'deliveraddressnumber' )->item(0)->textContent )
-				->setCustomer( $customer )
-				->setPeriod( $responseDOM->getElementsByTagName( 'period' )->item(0)->textContent )
-				->setCurrency( $responseDOM->getElementsByTagName( 'currency' )->item(0)->textContent )
-				->setStatus( $responseDOM->getElementsByTagName( 'status' )->item(0)->textContent )
-				->setPaymentMethod( $responseDOM->getElementsByTagName( 'paymentmethod' )->item(0)->textContent );
-
+		$lineTags = array(
+			'article' => 'setArticle',
+			'subarticle' => 'setSubArticle',
+			'quantity' => 'setQuantity',
+			'units' => 'setUnits',
+			'allowdiscountorpremium' => 'setAllowDiscountOrPremium',
+			'description' => 'setDescription',
+			'valueexcl' => 'setValueExcl',
+			'vatvalue' => 'setVatValue',
+			'valueinc' => 'setValueInc',
+			'unitspriceexcl' => 'setUnitsPriceExcl',
+			'freetext1' => 'setFreeText1',
+			'freetext2' => 'setFreeText2',
+			'freetext3' => 'setFreeText3'
+		);
+		
 		foreach ( $responseDOM->getElementsByTagName( 'line' ) as $lineDOM ) {
 			$temp_line = new InvoiceLine();
-			$temp_line
-					->setArticle( $lineDOM->getElementsByTagName( 'article' )->item(0)->textContent )
-					->setSubArticle( $lineDOM->getElementsByTagName( 'subarticle' )->item(0)->textContent )
-					->setQuantity( $lineDOM->getElementsByTagName( 'quantity' )->item(0)->textContent )
-					->setUnits( $lineDOM->getElementsByTagName( 'units' )->item(0)->textContent )
-					->setAllowDiscountOrPremium( $lineDOM->getElementsByTagName( 'allowdiscountorpremium' )->item(0)->textContent )
-					->setDescription( $lineDOM->getElementsByTagName( 'description' )->item(0)->textContent )
-					->setValueExcl( $lineDOM->getElementsByTagName( 'valueexcl' )->item(0)->textContent )
-					->setVatValue( $lineDOM->getElementsByTagName( 'vatvalue' )->item(0)->textContent )
-					->setValueInc( $lineDOM->getElementsByTagName( 'valueinc' )->item(0)->textContent )
-					->setUnitsPriceExcl( $lineDOM->getElementsByTagName( 'unitspriceexcl' )->item(0)->textContent )
-					->setFreeText1( $lineDOM->getElementsByTagName( 'freetext1' )->item(0)->textContent )
-					->setFreeText2( $lineDOM->getElementsByTagName( 'freetext2' )->item(0)->textContent )
-					->setFreeText3( $lineDOM->getElementsByTagName( 'freetext3' )->item(0)->textContent );
-
+			
+			foreach($lineTags as $tag => $method) {
+				$_tag = $lineDOM->getElementsByTagName($tag)->item(0);
+				
+				if(isset($_tag) && isset($_tag->textContent))
+					$temp_line->$method($_tag->textContent);
+			}
+			
 			$invoice->addLine( $temp_line );
 			unset( $temp_line );
 		}
 
 		return $invoice;
-
-
-
-
-
-
 	}
 }
