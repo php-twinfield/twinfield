5efddb8a85c499bf64a468b130d500e95f093f90
Leon Rowland <leon@rowland.nl>
2013-05-03 08:50:45 +0200

    Addition of using createTextNode to ensure that utf8 characters are correctly used.  Fixes issues with ' " &

--- src/Pronamic/Twinfield/Customer/DOM/CustomersDocument.php
+++ src/Pronamic/Twinfield/Customer/DOM/CustomersDocument.php
@@ -17,23 +17,28 @@ class CustomersDocument extends \DOMDocument {
 
 	public function addCustomer(Customer $customer) {
 		// Makes code element
-		$codeElement = $this->createElement('code', $customer->getID());
+		$codeElement = $this->createElement('code');
+		$codeElement->appendChild($this->createTextNode($customer->getID()));
 		$this->dimensionElement->appendChild($codeElement);
 
 		// Make name element
-		$nameElement = $this->createElement('name', $customer->getName());
+		$nameElement = $this->createElement('name');
+		$nameElement->appendChild($this->createTextNode($customer->getName()));
 		$this->dimensionElement->appendChild($nameElement);
 
 		// Make type element
-		$typeElement = $this->createElement('type', $customer->getType());
+		$typeElement = $this->createElement('type');
+		$typeElement->appendChild($this->createTextNode($customer->getType()));
 		$this->dimensionElement->appendChild($typeElement);
 
 		// Make website element
-		$websiteElement = $this->createElement('website', $customer->getWebsite());
+		$websiteElement = $this->createElement('website');
+		$websiteElement->appendChild($this->createTextNode($customer->getWebsite()));
 		$this->dimensionElement->appendChild($websiteElement);
 		
 		// Test coc element
-		$cocNumberElement = $this->createElement('cocnumber', $customer->getCocNumber());
+		$cocNumberElement = $this->createElement('cocnumber');
+		$cocNumberElement->appendChild($this->createTextNode($customer->getCocNumber()));
 		$this->dimensionElement->appendChild($cocNumberElement);
 
 
@@ -43,12 +48,23 @@ class CustomersDocument extends \DOMDocument {
 			$this->dimensionElement->appendChild($financialElement);
 
 			// Set financial child elements
-			$dueDaysElement		 = $this->createElement('duedays', $customer->getDueDays());
-			$payAvailableElement = $this->createElement('payavailable', $customer->getPayAvailable());
-			$payCodeElement		 = $this->createElement('paycode', $customer->getPayCode());
-			$vatCodeElement		 = $this->createElement('vatcode', $customer->getVatCode());
-			$eBillingElement	 = $this->createElement('ebilling', $customer->getEBilling());
-			$eBillMailElement	 = $this->createElement('ebillmail', $customer->getEBillMail());
+			$dueDaysElement		 = $this->createElement('duedays');
+			$dueDaysElement->appendChild($this->createTextNode($customer->getDueDays()));
+			
+			$payAvailableElement = $this->createElement('payavailable');
+			$payAvailableElement->appendChild($this->createTextNode($customer->getPayAvailable()));
+			
+			$payCodeElement		 = $this->createElement('paycode');
+			$payCodeElement->appendChild($this->createTextNode($customer->getPayCode()));
+			
+			$vatCodeElement		 = $this->createElement('vatcode');
+			$vatCodeElement->appendChild($this->createTextNode($customer->getVatCode()));
+			
+			$eBillingElement	 = $this->createElement('ebilling');
+			$eBillingElement->appendChild($this->createTextNode($customer->getEBilling()));
+			
+			$eBillMailElement	 = $this->createElement('ebillmail');
+			$eBillMailElement->appendChild($this->createTextNode($customer->getEBillMail()));
 
 			// Add these to the financial element
 			$financialElement->appendChild($dueDaysElement);
@@ -63,7 +79,7 @@ class CustomersDocument extends \DOMDocument {
 		// Make address element
 		$addressesElement = $this->createElement('addresses');
 		$this->dimensionElement->appendChild($addressesElement);
-
+		
 		foreach($customer->getAddresses() as $address) {
 			// Makes new address element
 			$addressElement = $this->createElement('address');
@@ -72,21 +88,46 @@ class CustomersDocument extends \DOMDocument {
 			// Set attributes
 			$addressElement->setAttribute('default', $address->getDefault());
 			$addressElement->setAttribute('type', $address->getType());
-
+			
 			// Build elements
-			$aNameElement		 = $this->createElement( 'name', $address->getName() );
-			$countryElement		 = $this->createElement( 'country', $address->getCountry() );
-			$cityElement		 = $this->createElement( 'city', $address->getCity() );
-			$postcodeElement	 = $this->createElement( 'postcode', $address->getPostcode() );
-			$telephoneElement	 = $this->createElement( 'telephone', $address->getTelephone() );
-			$faxElement			 = $this->createElement( 'telefax', $address->getFax() );
-			$emailElement		 = $this->createElement( 'email', $address->getEmail() );
-			$field1Element		 = $this->createElement( 'field1', $address->getField1() );
-			$field2Element		 = $this->createElement( 'field2', $address->getField2() );
-			$field3Element		 = $this->createElement( 'field3', $address->getField3() );
-			$field4Element		 = $this->createElement( 'field4', $address->getField4() );
-			$field5Element		 = $this->createElement( 'field5', $address->getField5() );
-			$field6Element		 = $this->createElement( 'field6', $address->getField6() );
+			$aNameElement = $this->createElement('name');
+			$aNameElement->appendChild($this->createTextNode($address->getName()));
+			
+			$countryElement = $this->createElement( 'country');
+			$countryElement->appendChild($this->createTextNode($address->getCountry()));
+			
+			$cityElement = $this->createElement('city');
+			$cityElement->appendChild($this->createTextNode($address->getCity()));
+			
+			$postcodeElement = $this->createElement('postcode');
+			$postcodeElement->appendChild($this->createTextNode($address->getPostcode()));
+			
+			$telephoneElement = $this->createElement('telephone');
+			$telephoneElement->appendChild($this->createTextNode($address->getTelephone()));
+			
+			$faxElement = $this->createElement('telefax');
+			$faxElement->appendChild($this->createTextNode($address->getFax()));
+			
+			$emailElement = $this->createElement('email');
+			$emailElement->appendChild($this->createTextNode($address->getEmail()));
+			
+			$field1Element = $this->createElement('field1');
+			$field1Element->appendChild( $this->createTextNode($address->getField1()));
+			
+			$field2Element = $this->createElement('field2');
+			$field2Element->appendChild( $this->createTextNode($address->getField2()));
+			
+			$field3Element = $this->createElement('field3');
+			$field3Element->appendChild( $this->createTextNode($address->getField3()));
+			
+			$field4Element = $this->createElement('field4');
+			$field4Element->appendChild( $this->createTextNode($address->getField4()));
+			
+			$field5Element = $this->createElement('field5');
+			$field5Element->appendChild( $this->createTextNode($address->getField5()));
+			
+			$field6Element = $this->createElement('field6');
+			$field6Element->appendChild( $this->createTextNode($address->getField6()));
 
 			// Add these elements to the address
 			$addressElement->appendChild($aNameElement);
