ea15d14b6d5575a0f44144835e52af9a8dbd37cf
Leon Rowland <leon@rowland.nl>
2013-06-24 14:26:21 +0200

    more elements mapped to their objects

--- src/Pronamic/Twinfield/Invoice/Mapper/InvoiceMapper.php
+++ src/Pronamic/Twinfield/Invoice/Mapper/InvoiceMapper.php
@@ -13,27 +13,36 @@ class InvoiceMapper {
 		$responseDOM = $response->getResponseDocument();
 		
 		$invoiceTags = array(
-			'office' => 'setOffice',
-			'invoicetype' => 'setInvoiceType',
-			'invoicenumber' => 'setInvoiceNumber',
-			'invoicedate' => 'setInvoiceDate',
-			'duedate' => 'setDueDate',
-			'bank' => 'setBank',
-			'invoiceaddressnumber' => 'setInvoiceAddressNumber',
-			'deliveraddressnumber' => 'setDeliverAddressNumber',
-			'period' => 'setPeriod',
-			'currency' => 'setCurrency',
-			'status' => 'setStatus',
-			'paymentmethod' => 'setPaymentMethod'
+			'office'				 => 'setOffice',
+			'invoicetype'			 => 'setInvoiceType',
+			'invoicenumber'			 => 'setInvoiceNumber',
+			'status'				 => 'setStatus',
+			'currency'				 => 'setCurrency',
+			'period'				 => 'setPeriod',
+			'invoicedate'			 => 'setInvoiceDate',
+			'duedate'				 => 'setDueDate',
+			'performancedate'		 => 'setPerformanceDate',
+			'paymentmethod'			 => 'setPaymentMethod',
+			'bank'					 => 'setBank',
+			'invoiceaddressnumber'	 => 'setInvoiceAddressNumber',
+			'deliveraddressnumber'	 => 'setDeliverAddressNumber',
+			'headertext'			 => 'setHeaderText',
+			'footertext'			 => 'setFooterText'
 		);
 		
 		$customerTags = array(
 			'customer' => 'setID'
 		);
 		
+		$totalsTags = array(
+			'valueexcl'	 => 'setValueExcl',
+			'valueinc'	 => 'setValueInc'
+		);
+		
 		// Generate new Invoice
 		$invoice = new Invoice();
 		
+		// Loop through all invoice tags
 		foreach($invoiceTags as $tag => $method) {
 			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
 			
@@ -41,8 +50,8 @@ class InvoiceMapper {
 				$invoice->$method($_tag->textContent);
 		}
 		
+		// Make a custom, and loop through custom tags
 		$customer = new Customer();
-		
 		foreach($customerTags as $tag => $method) {
 			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
 			
@@ -50,7 +59,18 @@ class InvoiceMapper {
 				$customer->$method($_tag->textContent);
 		}
 		
+		// Make an InvoiceTotals and loop through custom tags
+		$invoiceTotals = new InvoiceTotals();
+		foreach($totalsTags as $tag => $method) {
+			$_tag = $responseDOM->getElementsByTagName($tag)->item(0);
+			
+			if(isset($_tag) && isset($_tag->textContent))
+				$invoiceTotals->$method($_tag->textContent);
+		}
+		
+		// Set the custom classes to the invoice
 		$invoice->setCustomer( $customer );
+		$invoice->setTotals($invoiceTotals);
 
 		$lineTags = array(
 			'article' => 'setArticle',
@@ -63,9 +83,11 @@ class InvoiceMapper {
 			'vatvalue' => 'setVatValue',
 			'valueinc' => 'setValueInc',
 			'unitspriceexcl' => 'setUnitsPriceExcl',
+			'unitspriceinc' => 'setUnitsPriceInc',
 			'freetext1' => 'setFreeText1',
 			'freetext2' => 'setFreeText2',
-			'freetext3' => 'setFreeText3'
+			'freetext3' => 'setFreeText3',
+			'performancedate' => 'setPerformanceDate'
 		);
 		
 		foreach ( $responseDOM->getElementsByTagName( 'line' ) as $lineDOM ) {
