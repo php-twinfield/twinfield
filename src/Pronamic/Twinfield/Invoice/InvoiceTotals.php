8d5a21d3978a870b5d42657a1c9f0b1dd44ab0e9
Leon Rowland <leon@rowland.nl>
2013-06-24 13:25:42 +0200

    Addition of InvoiceTotals class

---
+++ src/Pronamic/Twinfield/Invoice/InvoiceTotals.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Pronamic\Twinfield\Invoice;
+
+class InvoiceTotals {
+	
+	private $valueExcl;
+	private $valueInc;
+	
+	public function getValueExcl() {
+		return $this->valueExcl;
+	}
+
+	public function setValueExcl( $valueExcl ) {
+		$this->valueExcl = $valueExcl;
+	}
+
+	public function getValueInc() {
+		return $this->valueInc;
+	}
+
+	public function setValueInc( $valueInc ) {
+		$this->valueInc = $valueInc;
+	}
+}
