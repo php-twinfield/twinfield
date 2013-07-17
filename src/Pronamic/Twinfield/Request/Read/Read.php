471de11860c93f13bc9bf0b2252f702f02256164
Leon Rowland <leon@rowland.nl>
2013-07-17 13:18:42 +0200

    psr standard

--- src/Pronamic/Twinfield/Request/Read/Read.php
+++ src/Pronamic/Twinfield/Request/Read/Read.php
@@ -1,22 +1,21 @@
 <?php
-
 namespace Pronamic\Twinfield\Request\Read;
 
-abstract class Read extends \DOMDocument {
+abstract class Read extends \DOMDocument
+{
+    private $readElement;
 
-	private $readElement;
+    public function __construct()
+    {
+        parent::__construct();
 
-	public function __construct() {
-		parent::__construct();
+        $this->readElement = $this->createElement('read');
+        $this->appendChild($this->readElement);
+    }
 
-		$this->readElement = $this->createElement( 'read' );
-		$this->appendChild( $this->readElement );
-	}
-
-	public function add( $element, $value ) {
-		$_element = $this->createElement( $element, $value );
-		$this->readElement->appendChild( $_element );
-	}
+    public function add($element, $value)
+    {
+        $_element = $this->createElement($element, $value);
+        $this->readElement->appendChild($_element);
+    }
 }
-
-
