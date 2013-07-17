fe2e5b54354d715b70be1df3bdf48980179576fe
Leon Rowland <leon@rowland.nl>
2013-07-17 12:56:01 +0200

    psr fix

--- src/Pronamic/Twinfield/Request/Catalog/Catalog.php
+++ src/Pronamic/Twinfield/Request/Catalog/Catalog.php
@@ -1,20 +1,21 @@
 <?php
-
 namespace Pronamic\Twinfield\Request\Catalog;
 
-abstract class Catalog extends \DOMDocument {
+abstract class Catalog extends \DOMDocument
+{
+    private $listElement;
 
-	private $listElement;
+    public function __construct()
+    {
+        parent::__construct();
 
-	public function __construct() {
-		parent::__construct();
+        $this->listElement = $this->createElement('list');
+        $this->appendChild($this->listElement);
+    }
 
-		$this->listElement = $this->createElement( 'list' );
-		$this->appendChild( $this->listElement );
-	}
-
-	protected function add( $element, $value ) {
-		$_element = $this->createElement( $element, $value );
-		$this->listElement->appendChild( $_element );
-	}
+    protected function add($element, $value)
+    {
+        $_element = $this->createElement($element, $value);
+        $this->listElement->appendChild($_element);
+    }
 }
