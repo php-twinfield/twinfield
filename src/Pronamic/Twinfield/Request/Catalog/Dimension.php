a3a9cda3d131a7043b222e0194581ae419afd39e
Leon Rowland <leon@rowland.nl>
2013-07-17 13:03:55 +0200

    psr fix

--- src/Pronamic/Twinfield/Request/Catalog/Dimension.php
+++ src/Pronamic/Twinfield/Request/Catalog/Dimension.php
@@ -1,23 +1,25 @@
 <?php
-
 namespace Pronamic\Twinfield\Request\Catalog;
 
-class Dimension extends Catalog {
+class Dimension extends Catalog
+{
 
-	public function __construct( $office = null, $dimType = null ) {
-		parent::__construct();
-		$this->add( 'type', 'dimensions' );
-		$this->setOffice( $office );
-		$this->setDimType( $dimType );
-	}
+    public function __construct($office = null, $dimType = null)
+    {
+        parent::__construct();
 
-	public function setOffice( $office ) {
-		$this->add( 'office', $office );
-	}
+        $this->add('type', 'dimensions');
+        $this->setOffice($office);
+        $this->setDimType($dimType);
+    }
 
-	public function setDimType( $dimType ) {
-		$this->add( 'dimtype', $dimType );
-	}
+    public function setOffice($office)
+    {
+        $this->add('office', $office);
+    }
 
-
+    public function setDimType($dimType)
+    {
+        $this->add('dimtype', $dimType);
+    }
 }
