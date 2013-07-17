f880d4860804d2041cb59774fe29d2f692f7ac24
Leon Rowland <leon@rowland.nl>
2013-07-17 13:12:51 +0200

    psr standard

--- src/Pronamic/Twinfield/Request/Catalog/Office.php
+++ src/Pronamic/Twinfield/Request/Catalog/Office.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace Pronamic\Twinfield\Request\Catalog;
 
-class Office extends Catalog {
-	public function __construct() {
-		$this->add( 'type', 'office' );
-	}
+class Office extends Catalog
+{
+    public function __construct()
+    {
+        $this->add('type', 'office');
+    }
 }
