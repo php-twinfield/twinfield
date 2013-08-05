5fe0640867d7325af9f960c7e8934ac61428a58f
Remco Tolsma <info@remcotolsma.nl>
2013-08-05 12:11:59 +0200

    Adjusted the autoload test, now use assertTrue.

--- tests/test-autoload.php
+++ tests/test-autoload.php
@@ -2,9 +2,9 @@
 
 class Pronamic_Twinfield_Autoload_Test extends PHPUnit_Framework_TestCase {
 	function test_autoload() {
-		$class_exists = class_exists( 'Pronamic\Twinfield\Secure\Login' );
+		$class_exists = class_exists( '\Pronamic\Twinfield\Secure\Login' );
 
 		// Assert
-		$this->assertEquals( $class_exists, true );
+		$this->assertTrue( $class_exists );
 	}
 }
