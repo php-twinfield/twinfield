4b1c04cf5d46446b483712623989426bbcd3d32a
Willem <MailCouponNL+github@gmail.com>
2014-05-27 16:47:43 +0200

    implemented send method

--- src/Pronamic/Twinfield/Article/ArticleFactory.php
+++ src/Pronamic/Twinfield/Article/ArticleFactory.php
@@ -86,106 +86,32 @@ class ArticleFactory extends ParentFactory
             }
         }
     }
-    
-    /**
-     * Requests all article from the List Dimension Type.
-     * 
-     * First attempts to login with the passed configuration into this
-     * instances constructor. If successful will get the Service class
-     * to handle further interactions.
-     * 
-     * Makes a new instance of Catalog\Dimension and sets the office and
-     * dimtype values.
-     * 
-     * Using the service class it will attempt to send the DOM document
-     * from Catalog\Dimension.
-     * 
-     * It sets the response to this instances method setResponse() (which you
-     * can access with getResponse())
-     * 
-     * If the response was successful it will loop through all the results
-     * in the response and make an array of the article ID as the array key
-     * and the value being an array of 'name' and 'shortname'
-     * 
-     * If the response wasn't succesful it will return false.
-     * 
-     * @access public
-     * @return array | false
-     */
-    public function listAll($office = null, $dimType = 'DEB')
-    {
-        // Attempts to process the login
-        if($this->getLogin()->process()) {
-            
-            // Gets the secure service class
-            $service = $this->getService();
-
-            // If no office present, use the config set value
-            if(! $office)
-                $office = $this->getConfig()->getOffice();
-            
-            // Make a request to a list of all customers
-            $request_customers = new Request\Catalog\Dimension(
-                $office, 
-                $dimType
-            );
-
-            // Send the Request document and set the response to this instance.
-            $response = $service->send($request_customers);
-            $this->setResponse($response);
-
-            // Loop through the results if successful
-            if($response->isSuccessful()) {
-
-                // Get the raw response document
-                $responseDOM = $response->getResponseDocument();
-
-                // Prepared empty customer array
-                $customers = array();
-
-                // Store in an array by customer id
-                foreach($responseDOM->getElementsByTagName('dimension') as $customer) {
-                    $customer_id = $customer->textContent;
-
-                    if(! is_numeric($customer_id))
-                        continue;
-
-                    $customers[$customer->textContent] = array(
-                        'name'      => $customer->getAttribute('name'),
-                        'shortName' => $customer->getAttribute('shortname')
-                    );
-                }
-
-                return $customers;
-            }
-        }
-    }
 
     /**
-     * Sends a \Pronamic\Twinfield\Customer\Customer instance to Twinfield
+     * Sends a \Pronamic\Twinfield\Article\Article instance to Twinfield
      * to update or add.
      * 
      * First attempts to login with the passed configuration in the constructor.
      * If successful will get the secure Service class.
      * 
      * It will then make an instance of 
-     * \Pronamic\Twinfield\Customer\DOM\CustomersDocument where it will
-     * pass in the Customer class in this methods parameter.
+     * \Pronamic\Twinfield\Customer\DOM\ArticleDocument where it will
+     * pass in the Article class in this methods parameter.
      * 
-     * It will then attempt to send the DOM document from CustomersDocument
+     * It will then attempt to send the DOM document from ArticlesDocument
      * and set the response to this instances method setResponse() (which you
      * can get with getResponse())
      * 
      * If successful will return true, else will return false.
      * 
-     * If you want to map the response back into a customer use getResponse()->
-     * getResponseDocument()->asXML() into the CustomerMapper::map method.
+     * If you want to map the response back into a Artoc;e use getResponse()->
+     * getResponseDocument()->asXML() into the ArticleMapper::map method.
      * 
      * @access public
-     * @param \Pronamic\Twinfield\Customer\Customer $customer
+     * @param \Pronamic\Twinfield\Article\Article $articl
      * @return boolean
      */
-    public function send(Customer $customer)
+    public function send(Article $article)
     {
         // Attempts the process login
         if($this->getLogin()->process()) {
@@ -193,12 +119,12 @@ class ArticleFactory extends ParentFactory
             // Gets the secure service
             $service = $this->getService();
 
-            // Gets a new instance of CustomersDocument and sets the $customer
-            $customersDocument = new DOM\CustomersDocument();
-            $customersDocument->addCustomer($customer);
+            // Gets a new instance of ArticlesDocument and sets the $article
+            $articlesDocument = new DOM\ArticlesDocument();
+            $articlesDocument->addArticle($article);
 
             // Send the DOM document request and set the response
-            $response = $service->send($customersDocument);
+            $response = $service->send($articlesDocument);
             $this->setResponse($response);
 
             // Return a bool on status of response.
@@ -208,36 +134,5 @@ class ArticleFactory extends ParentFactory
                 return false;
             }
         }
-    }
-    
-    /**
-     * Returns the very next free code available for the Customer Code.  As adding a new customer still requires
-     * a code.  This method first retrieves all customers, sorts by key, gets the last element, and increments the code
-     * by one.
-     * 
-     * @access public
-     * @return int
-     */
-    public function getFirstFreeCode() 
-    {
-        // Get all customers
-        $customers = $this->listAll();
-        
-        // Check some customers exist
-        if(empty($customers))
-            return 0;
-        
-        // Get the keys
-        $customersKeys = array_keys($customers);
-        
-        // Sort the keys and reverse to get the last first
-        asort($customersKeys);
-        $customersKeys = array_reverse($customersKeys);
-        
-        // Get the first of the reversed keys
-        $latestCustomerCode = $customersKeys[0];
-        
-        // Increment by one and return.
-        return (int) ++$latestCustomerCode;
     }
 }
