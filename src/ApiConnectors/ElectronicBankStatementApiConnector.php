a81aa4c6eef2dbec6200410c579f55bddf072c19
Willem Stuursma <willem@mollie.nl>
2017-12-04 16:36:20 +0100

    Implementation

--- src/ApiConnectors/ElectronicBankStatementApiConnector.php
+++ src/ApiConnectors/ElectronicBankStatementApiConnector.php
@@ -2,56 +2,43 @@
 
 namespace PhpTwinfield\ApiConnectors;
 
-use PhpTwinfield\Office;
+use PhpTwinfield\Customer;
+use PhpTwinfield\DomDocuments\CustomersDocument;
+use PhpTwinfield\DomDocuments\ElectronicBankStatementDocument;
+use PhpTwinfield\ElectronicBankStatement;
+use PhpTwinfield\Exception;
+use PhpTwinfield\Mappers\CustomerMapper;
+use PhpTwinfield\Request as Request;
 
 /**
  * A facade to make interaction with the the Twinfield service easier when trying to retrieve or send information about
- * offices.
- *
- * If you require more complex interactions or a heavier amount of control over the requests to/from then look inside
- * the methods or see the advanced guide detailing the required usages.
- *
- * @author Emile Bons <emile@emilebons.nl>
+ * Electronic Bank Statements.
  */
-class OfficeApiConnector extends BaseFinderApiConnector
+class ElectronicBankStatementApiConnector extends BaseApiConnector
 {
     /**
-     * List all offices.
+     * Sends an ElectronicBankStatement instance to Twinfield to update or add.
      *
-     * @param string $pattern  The search pattern. May contain wildcards * and ?
-     * @param int    $field    The search field determines which field or fields will be searched. The available fields
-     *                         depends on the finder type. Passing a value outside the specified values will cause an
-     *                         error.
-     * @param int    $firstRow First row to return, useful for paging
-     * @param int    $maxRows  Maximum number of rows to return, useful for paging
-     * @param array  $options  The Finder options. Passing an unsupported name or value causes an error. It's possible
-     *                         to add multiple options. An option name may be used once, specifying an option multiple
-     *                         times will cause an error.
-     *
-     * @return Office[] The offices found.
+     * @throws Exception
      */
-    public function listAll(
-        string $pattern = '*',
-        int $field = 0,
-        int $firstRow = 1,
-        int $maxRows = 100,
-        array $options = []
-    ): array {
-        $response = $this->searchFinder(self::TYPE_OFFICES, $pattern, $field, $firstRow, $maxRows, $options);
+    public function send(ElectronicBankStatement $electronicBankStatement): void
+    {
+        // Attempts the process login
+        if ($this->getLogin()->process()) {
+            // Gets the secure service
+            $service = $this->createService();
 
-        if ($response->data->TotalRows == 0) {
-            return [];
+            // Gets a new instance of CustomersDocument and sets the $customer
+            $ebsDocument = new ElectronicBankStatementDocument();
+            $ebsDocument->addStatement($electronicBankStatement);
+
+            // Send the DOM document request and set the response
+            $response = $service->send($ebsDocument);
+            $this->setResponse($response);
+
+            if (!$response->isSuccessful()) {
+                throw new Exception(implode(", ", $response->getErrorMessages()));
+            }
         }
-
-        $offices = [];
-        foreach ($response->data->Items->ArrayOfString as $officeArray) {
-            $office = new Office();
-            $office->setCode($officeArray->string[0]);
-            $office->setCountryCode($officeArray->string[2]);
-            $office->setName($officeArray->string[1]);
-            $offices[] = $office;
-        }
-
-        return $offices;
     }
 }
