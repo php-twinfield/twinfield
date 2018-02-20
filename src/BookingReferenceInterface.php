47d29b395b0f770fab3e36bec64caf5f1a161034
Thijs Riezebeek <thijs@mollie.com>
2018-02-20 11:11:09 +0100

    Add default interface implementations to docblocks

--- src/BookingReferenceInterface.php
+++ src/BookingReferenceInterface.php
@@ -6,8 +6,10 @@ namespace PhpTwinfield;
  * The values provided by this interface uniquely define a booking (e.g. SalesTransaction, BankTransaction, etc.) in
  * Twinfield and can be used when a reference to the booking is needed, for example when deleting a booking.
  *
- * You can use the provided MatchReference object or you can implement this directly on one of your entities / domain
+ * You can use the provided BookingReference object or you can implement this directly on one of your entities / domain
  * models.
+ *
+ * @see BookingReference
  */
 interface BookingReferenceInterface
 {
