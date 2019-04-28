3bef51b79e258480b5b96bd18d9208f79ea13e60
iranl <iranl@users.noreply.github.com>
2019-04-28 21:46:37 +0200

    Add files via upload

--- src/Article.php
+++ src/Article.php
@@ -9,26 +9,68 @@ use PhpTwinfield\Transactions\TransactionLineFields\VatCodeField;
  * @see https://c3.twinfield.com/webservices/documentation/#/ApiReference/Masters/Articles
  * @todo Add documentation and typehints to all properties.
  */
-class Article
+class Article extends BaseObject
 {
-    use VatCodeField;
     use OfficeField;
+    use VatCodeField;
 
+    private $allowChangeUnitsPrice = false;
+    private $allowChangeVatCode = false;
+    private $allowDecimalQuantity = false;
+    private $allowDiscountorPremium = true;
     private $code;
+    private $name;
+    private $percentage;
+    private $shortName;
     private $status;
     private $type;
-    private $name;
-    private $shortName;
-    private $unitNameSingular;
     private $unitNamePlural;
-    private $allowChangeVatCode = false;
-    private $performanceType;
-    private $allowChangePerformanceType;
-    private $percentage;
-    private $allowDiscountorPremium = true;
-    private $allowChangeUnitsPrice = false;
-    private $allowDecimalQuantity = false;
+    private $unitNameSingular;
     private $lines = [];
+
+    public function getAllowChangeUnitsPrice(): bool
+    {
+        return $this->allowChangeUnitsPrice;
+    }
+
+    public function setAllowChangeUnitsPrice(bool $allowChangeUnitsPrice): self
+    {
+        $this->allowChangeUnitsPrice = $allowChangeUnitsPrice;
+        return $this;
+    }
+
+    public function getAllowChangeVatCode(): bool
+    {
+        return $this->allowChangeVatCode;
+    }
+
+    public function setAllowChangeVatCode(bool $allowChangeVatCode): self
+    {
+        $this->allowChangeVatCode = $allowChangeVatCode;
+        return $this;
+    }
+
+    public function getAllowDecimalQuantity(): bool
+    {
+        return $this->allowDecimalQuantity;
+    }
+
+    public function setAllowDecimalQuantity(bool $allowDecimalQuantity): self
+    {
+        $this->allowDecimalQuantity = $allowDecimalQuantity;
+        return $this;
+    }
+
+    public function getAllowDiscountorPremium(): bool
+    {
+        return $this->allowDiscountorPremium;
+    }
+
+    public function setAllowDiscountorPremium(bool $allowDiscountorPremium): self
+    {
+        $this->allowDiscountorPremium = $allowDiscountorPremium;
+        return $this;
+    }
 
     public function getCode()
     {
@@ -38,6 +80,39 @@ class Article
     public function setCode($code)
     {
         $this->code = $code;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getPercentage()
+    {
+        return $this->percentage;
+    }
+
+    public function setPercentage($percentage)
+    {
+        $this->percentage = $percentage;
+        return $this;
+    }
+
+    public function getShortName()
+    {
+        return $this->shortName;
+    }
+
+    public function setShortName($shortName)
+    {
+        $this->shortName = $shortName;
         return $this;
     }
 
@@ -63,39 +138,6 @@ class Article
         return $this;
     }
 
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function setName($name)
-    {
-        $this->name = $name;
-        return $this;
-    }
-
-    public function getShortName()
-    {
-        return $this->shortName;
-    }
-
-    public function setShortName($shortName)
-    {
-        $this->shortName = $shortName;
-        return $this;
-    }
-
-    public function getUnitNameSingular()
-    {
-        return $this->unitNameSingular;
-    }
-
-    public function setUnitNameSingular($unitNameSingular)
-    {
-        $this->unitNameSingular = $unitNameSingular;
-        return $this;
-    }
-
     public function getUnitNamePlural()
     {
         return $this->unitNamePlural;
@@ -107,81 +149,14 @@ class Article
         return $this;
     }
 
-    public function getAllowChangeVatCode(): bool
+    public function getUnitNameSingular()
     {
-        return $this->allowChangeVatCode;
+        return $this->unitNameSingular;
     }
 
-    public function setAllowChangeVatCode(bool $allowChangeVatCode): self
+    public function setUnitNameSingular($unitNameSingular)
     {
-        $this->allowChangeVatCode = $allowChangeVatCode;
-        return $this;
-    }
-
-    public function getPerformanceType()
-    {
-        return $this->performanceType;
-    }
-
-    public function setPerformanceType($performanceType)
-    {
-        $this->performanceType = $performanceType;
-        return $this;
-    }
-
-    public function getAllowChangePerformanceType()
-    {
-        return $this->allowChangePerformanceType;
-    }
-
-    public function setAllowChangePerformanceType($allowChangePerformanceType)
-    {
-        $this->allowChangePerformanceType
-            = $allowChangePerformanceType;
-        return $this;
-    }
-
-    public function getPercentage()
-    {
-        return $this->percentage;
-    }
-
-    public function setPercentage($percentage)
-    {
-        $this->percentage = $percentage;
-        return $this;
-    }
-
-    public function getAllowDiscountorPremium(): bool
-    {
-        return $this->allowDiscountorPremium;
-    }
-
-    public function setAllowDiscountorPremium(bool $allowDiscountorPremium): self
-    {
-        $this->allowDiscountorPremium = $allowDiscountorPremium;
-        return $this;
-    }
-
-    public function getAllowChangeUnitsPrice(): bool
-    {
-        return $this->allowChangeUnitsPrice;
-    }
-
-    public function setAllowChangeUnitsPrice(bool $allowChangeUnitsPrice): self
-    {
-        $this->allowChangeUnitsPrice = $allowChangeUnitsPrice;
-        return $this;
-    }
-
-    public function getAllowDecimalQuantity(): bool
-    {
-        return $this->allowDecimalQuantity;
-    }
-
-    public function setAllowDecimalQuantity(bool $allowDecimalQuantity): self
-    {
-        $this->allowDecimalQuantity = $allowDecimalQuantity;
+        $this->unitNameSingular = $unitNameSingular;
         return $this;
     }
 
--- src/ArticleLine.php
+++ src/ArticleLine.php
@@ -8,20 +8,55 @@ namespace PhpTwinfield;
  */
 class ArticleLine
 {
+    private $freeText1;
+    private $freeText2;
+    private $freeText3;
     private $ID;
-    private $status;
     private $inUse;
-    private $unitsPriceExcl;
-    private $unitsPriceInc;
-    private $units;
     private $name;
     private $shortName;
+    private $status;
     private $subCode;
-    private $freeText1;
+    private $units;
+    private $unitsPriceExcl;
+    private $unitsPriceInc;
 
     public function __construct()
     {
         $this->ID = uniqid();
+    }
+
+    public function getFreeText1()
+    {
+        return $this->freeText1;
+    }
+
+    public function setFreeText1($freeText1)
+    {
+        $this->freeText1 = $freeText1;
+        return $this;
+    }
+
+    public function getFreeText2()
+    {
+        return $this->freeText2;
+    }
+
+    public function setFreeText2($freeText2)
+    {
+        $this->freeText2 = $freeText2;
+        return $this;
+    }
+
+    public function getFreeText3()
+    {
+        return $this->freeText3;
+    }
+
+    public function setFreeText3($freeText3)
+    {
+        $this->freeText3 = $freeText3;
+        return $this;
     }
 
     public function getID()
@@ -35,17 +70,6 @@ class ArticleLine
         return $this;
     }
 
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    public function setStatus($status)
-    {
-        $this->status = $status;
-        return $this;
-    }
-
     public function getInUse()
     {
         return $this->inUse;
@@ -54,39 +78,6 @@ class ArticleLine
     public function setInUse($inUse)
     {
         $this->inUse = $inUse;
-        return $this;
-    }
-
-    public function getUnitsPriceInc()
-    {
-        return $this->unitsPriceInc;
-    }
-
-    public function setUnitsPriceInc($unitsPriceInc)
-    {
-        $this->unitsPriceInc = $unitsPriceInc;
-        return $this;
-    }
-
-    public function getUnitsPriceExcl ()
-    {
-        return $this->unitsPriceExcl;
-    }
-
-    public function setUnitsPriceExcl($unitsPriceExcl)
-    {
-        $this->unitsPriceExcl = $unitsPriceExcl;
-        return $this;
-    }
-
-    public function getUnits()
-    {
-        return $this->units;
-    }
-
-    public function setUnits($units)
-    {
-        $this->units = $units;
         return $this;
     }
 
@@ -112,6 +103,17 @@ class ArticleLine
         return $this;
     }
 
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
     public function getSubCode()
     {
         return $this->subCode;
@@ -123,14 +125,36 @@ class ArticleLine
         return $this;
     }
 
-    public function getFreeText1()
+    public function getUnits()
     {
-        return $this->freeText1;
+        return $this->units;
     }
 
-    public function setFreeText1($freeText1)
+    public function setUnits($units)
     {
-        $this->freeText1 = $freeText1;
+        $this->units = $units;
+        return $this;
+    }
+
+    public function getUnitsPriceExcl()
+    {
+        return $this->unitsPriceExcl;
+    }
+
+    public function setUnitsPriceExcl($unitsPriceExcl)
+    {
+        $this->unitsPriceExcl = $unitsPriceExcl;
+        return $this;
+    }
+
+    public function getUnitsPriceInc()
+    {
+        return $this->unitsPriceInc;
+    }
+
+    public function setUnitsPriceInc($unitsPriceInc)
+    {
+        $this->unitsPriceInc = $unitsPriceInc;
         return $this;
     }
 }
