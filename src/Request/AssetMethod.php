223ae74bcd7f8dbd42ce80a049cb9ecbbc1d8080
iranl <iranl@users.noreply.github.com>
2019-05-09 17:20:05 +0200

    Add files via upload

---
+++ src/Request/AssetMethod.php
@@ -0,0 +1,90 @@
+<?php
+namespace PhpTwinfield\Request;
+
+use DOMDocument;
+use DOMElement;
+
+/**
+ * Used to request a specific AssetMethod from a certain office and code.
+ *
+ * @package PhpTwinfield
+ * @subpackage Request
+ * @author Yannick Aerssens <y.r.aerssens@gmail.com>
+ * @version 0.0.1
+ */
+class AssetMethod extends DOMDocument
+{
+    /**
+     * Holds the <assetmethod> element that all
+     * additional elements should be a child of.
+     *
+     * @access private
+     * @var DOMElement
+     */
+    private $assetMethodElement;
+
+    /**
+     * Creates the <assetmethod> element and adds it to the property
+     * assetMethodElement and sets office and code if they are present.
+     *
+     * @access public
+     */
+    public function __construct($office = null, $code = null)
+    {
+        parent::__construct();
+
+        $this->assetMethodElement = $this->createElement('assetmethod');
+        $this->appendChild($this->assetMethodElement);
+
+        if (null !== $office) {
+            $this->setOffice($office);
+        }
+
+        if (null !== $code) {
+            $this->setCode($code);
+        }
+    }
+
+    /**
+     * Adds additional elements to the <assetmethod> dom element.
+     *
+     * See the documentation over what <assetmethod> requires to know
+     * and what additional elements you need.
+     *
+     * @access protected
+     * @param string $element
+     * @param mixed $value
+     * @return void
+     */
+    protected function add($element, $value)
+    {
+        $_element = $this->createElement($element, $value);
+        $this->assetMethodElement->appendChild($_element);
+    }
+
+    /**
+     * Sets the office code for this request.
+     *
+     * @access public
+     * @param int $office
+     * @return \PhpTwinfield\Request
+     */
+    public function setOffice($office)
+    {
+        $this->add('office', $office);
+        return $this;
+    }
+
+    /**
+     * Sets the code for this request.
+     *
+     * @access public
+     * @param string $code
+     * @return \PhpTwinfield\Request
+     */
+    public function setCode($code)
+    {
+        $this->add('code', $code);
+        return $this;
+    }
+}
---
+++ src/Request/DimensionType.php
@@ -0,0 +1,90 @@
+<?php
+namespace PhpTwinfield\Request;
+
+use DOMDocument;
+use DOMElement;
+
+/**
+ * Used to request a specific DimensionType from a certain office and code.
+ *
+ * @package PhpTwinfield
+ * @subpackage Request
+ * @author Yannick Aerssens <y.r.aerssens@gmail.com>
+ * @version 0.0.1
+ */
+class DimensionType extends DOMDocument
+{
+    /**
+     * Holds the <dimensiontype> element that all
+     * additional elements should be a child of.
+     *
+     * @access private
+     * @var DOMElement
+     */
+    private $dimensionTypeElement;
+
+    /**
+     * Creates the <dimensiontype> element and adds it to the property
+     * dimensionTypeElement and sets office and code if they are present.
+     *
+     * @access public
+     */
+    public function __construct($office = null, $code = null)
+    {
+        parent::__construct();
+
+        $this->dimensionTypeElement = $this->createElement('dimensiontype');
+        $this->appendChild($this->dimensionTypeElement);
+
+        if (null !== $office) {
+            $this->setOffice($office);
+        }
+
+        if (null !== $code) {
+            $this->setCode($code);
+        }
+    }
+
+    /**
+     * Adds additional elements to the <dimensiontype> dom element.
+     *
+     * See the documentation over what <dimensiontype> requires to know
+     * and what additional elements you need.
+     *
+     * @access protected
+     * @param string $element
+     * @param mixed $value
+     * @return void
+     */
+    protected function add($element, $value)
+    {
+        $_element = $this->createElement($element, $value);
+        $this->dimensionTypeElement->appendChild($_element);
+    }
+
+    /**
+     * Sets the office code for this request.
+     *
+     * @access public
+     * @param int $office
+     * @return \PhpTwinfield\Request
+     */
+    public function setOffice($office)
+    {
+        $this->add('office', $office);
+        return $this;
+    }
+
+    /**
+     * Sets the code for this request.
+     *
+     * @access public
+     * @param string $code
+     * @return \PhpTwinfield\Request
+     */
+    public function setCode($code)
+    {
+        $this->add('code', $code);
+        return $this;
+    }
+}
