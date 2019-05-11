f09fe2de78e2525bb44516e8d2ac0196d46d4b52
iranl <iranl@users.noreply.github.com>
2019-05-11 12:03:41 +0200

    Add files via upload

--- src/DomDocuments/InvoicesDocument.php
+++ src/DomDocuments/InvoicesDocument.php
@@ -115,7 +115,7 @@ class InvoicesDocument extends BaseDocument
 
                 $article = $articleApiConnector->get($line->getArticleToCode(), $invoice->getOffice());
 
-                if ($article->getAllowChangeVatCode == true) {
+                if ($article->getAllowChangeVatCode() == true) {
                     $lineElement->appendChild($this->createNodeWithTextContent('vatcode', $line->getVatCodeToCode()));
                 }
             }
