6b5c4470c6a569d2cd137027124d3b399853dd7b
iranl <iranl@users.noreply.github.com>
2019-05-11 12:29:34 +0200

    Add files via upload

--- src/Fields/PeriodField.php
+++ src/Fields/PeriodField.php
@@ -5,8 +5,7 @@ namespace PhpTwinfield\Fields;
 trait PeriodField
 {
     /**
-     * Period in 'YYYY/PP' format (e.g. '2013/05'). If this tag is not included or if it is left empty, the period is
-     * determined by the system based on the provided transaction date.
+     * Period in 'YYYY/PP' format (e.g. '2013/05')
      * Used by: FixedAssetTransactionLine, Invoice
      *
      * @var string
@@ -28,7 +27,7 @@ trait PeriodField
     public function setPeriod(?string $period): self
     {
         if (!preg_match("!\\d{4}/\\d{1,2}!", $period)) {
-            throw new \InvalidArgumentException("Period must be in YYYY/PP format (got: {$period}.");
+            $period = '';
         }
 
         $this->period = $period;
