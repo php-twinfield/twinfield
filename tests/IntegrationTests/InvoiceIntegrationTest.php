50ef823a9206c015f83cb668c0061f92416c9094
iranl <iranl@users.noreply.github.com>
2019-05-11 10:58:45 +0200

    Update InvoiceIntegrationTest.php

--- tests/IntegrationTests/InvoiceIntegrationTest.php
+++ tests/IntegrationTests/InvoiceIntegrationTest.php
@@ -176,7 +176,7 @@ class InvoiceIntegrationTest extends BaseIntegrationTest
     {
         
         $invoiceTypeApiConnector = $this->getMockBuilder('\PhpTwinfield\ApiConnectors\InvoiceTypeApiConnector')
-            ->setConstructorArgs($this->connection)
+            ->setConstructorArgs(array($this->connection))
             ->getMock();
         
         $invoiceTypeApiConnector->method("getInvoiceTypeVatType")->willReturn('exclusive');
