ff10cfd557d6d4115eb3f006754b4a51bc70b01f
iranl <iranl@users.noreply.github.com>
2019-06-10 17:35:19 +0200

    Update Read.php

--- src/Request/Read/Read.php
+++ src/Request/Read/Read.php
@@ -65,7 +65,7 @@ abstract class Read extends \DOMDocument
      * Sets the office code for this request.
      *
      * @access public
-     * @param int $office
+     * @param Office $office
      * @return \PhpTwinfield\Request\Read\Read
      */
     public function setOffice(Office $office)
