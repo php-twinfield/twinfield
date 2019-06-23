314a9971ae8111bd8400ba3eb7d29bd10b636345
iranl <iranl@users.noreply.github.com>
2019-06-23 12:41:02 +0200

    Update OfficeIntegrationTest.php

--- tests/IntegrationTests/OfficeIntegrationTest.php
+++ tests/IntegrationTests/OfficeIntegrationTest.php
@@ -3,27 +3,76 @@
 namespace PhpTwinfield\IntegrationTests;
 
 use PhpTwinfield\ApiConnectors\OfficeApiConnector;
+use PhpTwinfield\Enums\Services;
+use PhpTwinfield\Office;
 use PhpTwinfield\Response\Response;
+use PhpTwinfield\Secure\AuthenticatedConnection;
+use PhpTwinfield\Services\FinderService;
+use PhpTwinfield\Services\ProcessXmlService;
+use PHPUnit\Framework\TestCase;
 
 /**
  * @runTestsInSeparateProcesses
  * @preserveGlobalState disabled
  */
-class OfficeIntegrationTest extends BaseIntegrationTest
+class OfficeIntegrationTest extends TestCase
 {
     /**
      * @var OfficeApiConnector|\PHPUnit_Framework_MockObject_MockObject
      */
     private $officeApiConnector;
+    
+    /**
+     * @var Office
+     */
+    protected $office;
+
+    /**
+     * @var AuthenticatedConnection|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $connection;
+
+    /**
+     * @var ProcessXmlService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $processXmlService;
+
+    /**
+     * @var FinderService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $finderService;
 
     protected function setUp()
     {
         parent::setUp();
 
-        unset($mockOfficeApiConnector);
-        \Mockery::close();
+        $this->office = new Office();
+        $this->office->setCode("11024");
 
+        $this->processXmlService = $this->createPartialMock(ProcessXmlService::class, ['sendDocument']);
+        $this->finderService     = $this->createPartialMock(FinderService::class, ['searchFinder']);
+
+        $this->connection  = $this->createMock(AuthenticatedConnection::class);
+        $this->connection->expects($this->any())
+            ->method("getAuthenticatedClient")
+            ->willReturnCallback(function (Services $service) {
+                switch ($service->getValue()) {
+                    case Services::PROCESSXML()->getValue():
+                        return $this->processXmlService;
+
+                    case Services::FINDER()->getValue():
+                        return $this->finderService;
+                }
+
+                throw new \InvalidArgumentException("Unknown service {$service->getValue()}");
+            });
+            
         $this->officeApiConnector = new OfficeApiConnector($this->connection);
+    }
+
+    protected function getSuccessfulResponse(): Response
+    {
+        return Response::fromString('<dimension result="1" />');
     }
 
     public function testListOfficesWithoutCompanyId()
