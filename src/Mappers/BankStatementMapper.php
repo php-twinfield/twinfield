3e1f873aff7ba25a17babcdec018fd4d18ec1573
bHosted <marc@vdgeijn.com>
2022-01-12 00:15:39 +0100

    Code cleanup and fix for crash when no statements are received

--- src/Mappers/BankStatementMapper.php
+++ src/Mappers/BankStatementMapper.php
@@ -3,29 +3,21 @@ namespace PhpTwinfield\Mappers;
 
 use Illuminate\Support\Collection;
 use PhpTwinfield\BankStatement;
-use PhpTwinfield\BankTransaction;
-use PhpTwinfield\Enums\DebitCredit;
-use PhpTwinfield\Enums\LineType;
-use PhpTwinfield\Enums\PerformanceType;
-use PhpTwinfield\Office;
-use PhpTwinfield\Transactions\BankTransactionLine\Base;
-use PhpTwinfield\Transactions\BankTransactionLine\Detail;
-use PhpTwinfield\Transactions\BankTransactionLine\Total;
-use PhpTwinfield\Transactions\BankTransactionLine\Vat;
-use PhpTwinfield\Util;
+use stdClass;
 
 class BankStatementMapper extends BaseMapper
 {
     /**
      * @throws \PhpTwinfield\Exception
      */
-    public static function map(\stdClass $bankStatements): Collection
+    public static function map(stdClass $bankStatements): Collection
     {
         $statements = new Collection();
 
         foreach( $bankStatements as $bankStatement )
         {
-            $statements[] = new BankStatement( $bankStatement->BankStatement );
+            if( property_exists( $bankStatement, 'BankStatement' ) )
+                $statements[] = new BankStatement( $bankStatement->BankStatement );
         }
 
         return $statements;
